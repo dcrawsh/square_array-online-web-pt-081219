db26ad7d288a686c133d8b9609d2719b8aba73b0
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-17 23:06:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,9 @@
 array = [1, 2, 3]
 new_array = []
 def square_array(array)
+  new_array= []
   array.each do |square|
   new_array.push(square ** 2)
   
 end    
 end
-
-new_array=square_array(array)
-puts new_array.inspect
